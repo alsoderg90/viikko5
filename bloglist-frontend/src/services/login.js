a566cf22665a509281261fee81c5d74b71c09f34
alexander <alsoderg@student.jyu.fi>
2020-09-10 05:13:28 +0300

    eslint korjaukset

--- bloglist-frontend/src/services/login.js
+++ bloglist-frontend/src/services/login.js
@@ -4,10 +4,10 @@ const baseUrl = '/api/login'
 
 
 const login = async credentials => {
-    const response = await axios.post(baseUrl, credentials)
-    console.log(response.data)
-    return response.data
+  const response = await axios.post(baseUrl, credentials)
+  console.log(response.data)
+  return response.data
 
 }
 
-export default {login}
+export default { login }
