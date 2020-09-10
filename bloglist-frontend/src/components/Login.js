4db6c84f3c28ca0e177877647263eb73e232eb50
alexander <alsoderg@student.jyu.fi>
2020-09-10 05:02:47 +0300

    5.0-5.12

--- bloglist-frontend/src/components/Login.js
+++ bloglist-frontend/src/components/Login.js
@@ -35,9 +35,9 @@ const LoginForm = ({
 }
 
 LoginForm.propTypes = {
-  handleSubmit: PropTypes.func.isRequired,
-  handleUsernameChange: PropTypes.func.isRequired,
-  handlePasswordChange: PropTypes.func.isRequired,
+  handleLogin: PropTypes.func.isRequired,
+  setUsername: PropTypes.func.isRequired,
+  setPassword: PropTypes.func.isRequired,
   username: PropTypes.string.isRequired,
   password: PropTypes.string.isRequired
 }
